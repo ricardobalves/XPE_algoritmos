c9cf4781529583d20d5fcbf814a3887aa880a085
ricardobalves <57296928+ricardobalves@users.noreply.github.com>
2023-08-01 08:35:56 -0300

    Add files via upload

---
+++ XPeAlgoritmos_EstruturaDados/Array/Metodos_Array.py
@@ -0,0 +1,42 @@
+# Program to demonstrate array methods
+
+# Create an array
+my_array = [1, 2, 3, 4, 5]
+# Print the array
+print(my_array)
+
+# Append an element
+my_array.append(6)
+# Print the array
+print(my_array)
+
+# Pop the last element
+my_array.pop()
+# Print the array
+print(my_array)
+
+# Index of an element
+index = my_array.index(3)
+# Print the index
+print(index)
+
+# Reverse the array
+my_array.reverse()
+# Print the array
+print(my_array)
+
+# Sort the array
+my_array.sort()
+# Print the array
+print(my_array)
+
+# incluir
+#clear()
+#copy()
+#count()
+#extend()
+#remove()
+
+#%%
+
+#%%
---
+++ XPeAlgoritmos_EstruturaDados/Sets/RemoveError.py
@@ -0,0 +1,16 @@
+nums = set([1, 2, 2, 3, 3, 3])
+
+print (nums)
+
+nums.remove(4)
+
+
+#%%
+nums = set([1, 2, 2, 3, 3, 3])
+
+print (nums)
+
+nums.discard(2)
+nums.discard(4)
+
+print (nums)
